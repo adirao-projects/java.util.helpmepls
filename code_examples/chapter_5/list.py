8723d28d07918a0d2fb04cf9e1cb3e441fcbcb1f
adirao-projects <adi.rao@mail.utoronto.ca>
2022-12-08 19:41:34 -0500

    procrastinating on work

---
+++ code_examples/chapter_5/list.py
@@ -0,0 +1,12 @@
+# Creating a list in Python
+
+# The following is a list of integers
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
+
+
+# The following is a list of characters
+lst_2 = ['A', 'B', 'C', 'D']
+
+
+# The following is a list of multiple data types
+lst_3 = [1, 2.3, 'A', 'Word', True]
