55414606c23ceef4b61e0539c9028082fe9248ba
Adi Rao <adityakrao.projects@gmail.com>
2023-10-18 19:44:13 -0400

    Bunch of fixes

---
+++ code_examples/chapter_5/dictionary_def.py
@@ -0,0 +1,22 @@
+# Creating a set in Python
+
+# The following is a dictionary of integers keys and character values
+dict_1 = {1: 'A', 
+          2: 'B',
+          3: 'C',
+          4: 'D',
+          5: 'E' } 
+
+# The following is a dictionary of character keys and integer values
+dict_2 = {'A': 100, 
+          'B': 1,
+          'C': 3,
+          'D': 12,
+          'E': 24 } 
+
+# The following is a dictionary of tuple keys and random values
+dict_3 = {(1, 2): 100,
+          (2, 1): 525,
+          (0, 0): 125,
+          (5, 1): 'ksrar',
+          (5, 1): True}
---
+++ code_examples/chapter_5/set_def.py
@@ -0,0 +1,10 @@
+# Creating a set in Python
+
+# The following is a set of characters
+set_1 = {'A', 'B', 'C', 'D', 'E' } 
+
+# The following is a set of integers
+set_2 = {100, 1, 3, 12, 24 } 
+
+# The following is a set of random data types
+set_3 = {(1, 2), 100, 525, 125, 'ksrar', True}
