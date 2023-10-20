905cf731ee06c97e4fea377f8723859fceea9643
Adi Rao <adityakrao.projects@gmail.com>
2023-10-20 13:52:09 -0400

    while loops

---
+++ code_examples/chapter_4/while_def.py
@@ -0,0 +1,11 @@
+# Creating a while loop
+
+# Setting a counter variable
+count = 0 
+
+# while boolean statement is true, print "Hello World"
+while count < 5:
+    print("Hello World")
+
+    # Increase counter variable by + 1 to avoid an infinite loop
+    count = count + 1
